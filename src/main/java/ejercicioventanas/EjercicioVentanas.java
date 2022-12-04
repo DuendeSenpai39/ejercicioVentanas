bc8f94d4cf61fb33c259a8acd891b45c84162911
Jose <Jose@DESKTOP-G0V731S>
2022-12-04 01:41:28 -0500

    Terminado TGP

---
+++ src/main/java/ejercicioventanas/EjercicioVentanas.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package ejercicioventanas;
+
+/**
+ *
+ * @author guama
+ */
+public class EjercicioVentanas {
+
+    public static void main(String[] args) {
+        Figura1 fig1 = new Figura1();
+        Figura2 fig2 = new Figura2();
+        Figura3 fig3 = new Figura3();
+        Figura4 fig4 = new Figura4();
+        
+        fig1.setVisible(true);
+        fig2.setVisible(true);
+        fig3.setVisible(true);
+        fig4.setVisible(true);
+        
+    }
+}
